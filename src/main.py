935c9adf615c31c272e47477f8209e29801d5d1b
EvoShady <danc1_cosmin@yahoo.com>
2021-07-22 18:34:46 +0300

    Updated code

--- src/main.py
+++ src/main.py
@@ -1,26 +1,34 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import to_date
-from pyspark.sql.types import IntegerType
+from pyspark.sql.types import IntegerType, StructField, StructType, StringType, DateType
 
 
 def main():
+    spark = start_spark_session()
+    schema = get_schema()
+    data_frame = spark.read.csv('data/myFile0.csv', header=True, dateFormat='dd-MM-yyyy', schema=schema)
+
+    data_frame.show()
+    data_frame.printSchema()
+
+
+def start_spark_session():
     spark = SparkSession \
         .builder \
         .master("local") \
         .appName('COVID-19 incidence rate in schools') \
         .config("spark.some.config.option", "some-value") \
         .getOrCreate()
+    return spark
 
-    # Read from csv file
-    data_frame = spark.read.option('multiline', 'true').csv('data/myFile0.csv', header=True, dateFormat='dd-MM-yyyy')
-    # Set column types
-    data_frame = data_frame.withColumn('Elementary School Cases', data_frame['Elementary School Cases'].cast(IntegerType()))
-    data_frame = data_frame.withColumn('Middle School Cases', data_frame['Middle School Cases'].cast(IntegerType()))
-    data_frame = data_frame.withColumn('High School Cases', data_frame['High School Cases'].cast(IntegerType()))
-    data_frame = data_frame.withColumn('Reporting Date', to_date(data_frame['Reporting Date'], 'dd-MM-yyyy'))
-    # Displays the content of the DataFrame to stdout
-    data_frame.show()
-    data_frame.printSchema()
+
+def get_schema():
+    schema = StructType([StructField('School Unit Name', StringType(), False),
+                         StructField('Elementary School Cases', IntegerType(), True),
+                         StructField('Middle School Cases', IntegerType(), True),
+                         StructField('High School Cases', IntegerType(), True),
+                         StructField('Gender', StringType(), True),
+                         StructField('Reporting Date', DateType(), True)])
+    return schema
 
 
 if __name__ == '__main__':
